b962f4d995f708d9a3257866dab91aa95b67e477
julian-pacilio <julian.pacilio.info@gmail.com>
2024-05-14 23:04:07 -0300

    update: Add custom color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,18 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    colors: {
+      "orange" : "#ff7d1a",
+      "pale-orange" : "#ffede0",
+      "very-dark-blue" : "#1d2025",
+      "dark-grayish-blue" : "#68707d",
+      "grayish-blue" : "#b6bcc8",
+      "ligth-grayish-blue" : "#f7f8fd",
+      "shadow" : "#000000bf",
+      "white" : "#fff"
+    },
+  },
   plugins: [],
 };
 export default config;
