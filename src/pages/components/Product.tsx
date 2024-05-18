f948a80aac36bc96147caa9a3f8e63b823f84f77
julian-pacilio <julian.pacilio.info@gmail.com>
2024-05-18 20:34:46 -0300

    update: Styles for desktop, Add images src

--- src/pages/components/Product.tsx
+++ src/pages/components/Product.tsx
@@ -1,40 +1,23 @@
 import Image from "next/image"
 
-import arrowNext from "../../../public/icon-next.svg"
-import arrowPrev from "../../../public/icon-previous.svg"
 import cartIcon from "../../../public/icon-cart.svg"
 import plusIcon from "../../../public/icon-plus.svg"
 import minusIcon from "../../../public/icon-minus.svg"
 
-import path from "../../../public/images/image-product-1.jpg"
+import ProductGallery from "./ui/ProductGallery"
+
+const imagesSrc = [
+    'image-product-1.jpg', 
+    'image-product-2.jpg', 
+    'image-product-3.jpg', 
+    'image-product-4.jpg'
+];
 
 export default function Product() {
     return(
-        <section className="flex flex-col">
-            <picture className="block relative">
-                <a className="flex justify-center items-center absolute left-4 top-1/2 bg-white rounded-full w-[40px] h-[40px]" href="javascript:void(0)">
-                    <Image
-                        src={arrowPrev}
-                        width={10} 
-                        alt="next-photo"
-                    />
-                </a>
-                <Image
-                    src={path}
-                    width={425}
-                    height={425}
-                    alt="next-photo"
-                />
-                <a className="flex justify-center items-center absolute right-4 top-1/2 bg-white rounded-full w-[40px] h-[40px]" 
-                href="javascript:void(0)">
-                    <Image 
-                        src={arrowNext} 
-                        width={10}
-                        alt="previous-photo" 
-                    />
-                </a>
-            </picture>
-            <div className="flex flex-col gap-5 p-6">
+        <section className="flex flex-col mx-auto max-w-[600px] lg:gap-20 lg:p-6 lg:flex-row lg:items-center lg:max-w-[1080px]">
+            <ProductGallery images={imagesSrc}/>
+            <div className="flex flex-col gap-5 p-6 lg:w-1/2">
                 <span className="block uppercase font-bold text-orange">Sneaker Company</span>
                 <h2 className="text-3xl font-bold
                 ">Fall Limited Edition Sneakers</h2>
@@ -43,15 +26,15 @@ export default function Product() {
                     durable rubber outer sole, they’ll withstand everything the weather can offer.              
                 </p>
                 <div className="flex flex-col gap-5">
-                    <div className="flex justify-between items-center">
+                    <div className="flex justify-between items-center lg:flex-col lg:items-start">
                         <div className="flex items-center gap-4">
                             <span className="text-3xl block font-bold">$125.00</span>
                             <span className="block font-bold text-orange bg-pale-orange rounded-md px-2 py-1">50%</span>
                         </div>
                         <span className="block font-bold text-dark-grayish-blue line-through">$250.00</span>
                     </div>
-                    <div className="buttons">
-                        <div className="flex items-center justify-between bg-ligth-grayish-blue rounded-md py-3 px-6">
+                    <div className="flex flex-col lg:flex-row gap-3">
+                        <div className="flex items-center justify-between bg-ligth-grayish-blue rounded-md py-3 px-6 w-full lg:w-[40%]">
                             <a href="#">
                             <Image
                                 src={minusIcon}
@@ -70,7 +53,7 @@ export default function Product() {
                             />
                             </a>
                         </div>
-                        <button className="flex justify-center gap-3 items-center font-semibold text-white bg-orange rounded-md py-3 mt-3 w-full">
+                        <button className="flex justify-center gap-3 items-center font-semibold text-white bg-orange rounded-md py-3 w-full lg:w-[60%]">
                             <Image
                                 src={cartIcon}
                                 width={18}
